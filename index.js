d480663d031ae3698ae339063e3989815d3b6bf1
Izhan <babuizhan1234@gmail.com>
2021-11-24 15:56:22 +0530

    Initialized repo

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) =>
+  res.status(200).send({
+    message: "Hello World!",
+  })
+);
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log("App is now running at port ", port);
+});
